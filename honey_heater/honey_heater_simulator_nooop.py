9dc2e2b14f6a49f38ea15385741e902ed2840fb0
PLSeuJ <jonathanschumann@posteo.de>
2022-07-05 00:28:02 +0200

    grid gets build correctly

--- honey_heater/honey_heater_simulator_nooop.py
+++ honey_heater/honey_heater_simulator_nooop.py
@@ -16,17 +16,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
 
-# number of gridpoints in directions [y, x]
-n_honey = [10, 10]
+# number of gridpoints in directions [x, y]
+n_honey = [10, 20]
 n_container = [5, 5]
 n_boundary = [1, 1]
 
 # distances in medium
-d_honey = [0.8, 0.4]
+d_honey = [0.4, 0.8]
 d_container = [2 * 10**(-5), 2 * 10**(-5)]
 d_boundary = [10**(-3), 10**(-3)]
 
-n, dd = [], []
+n = []
 for i in range(2):
     # sum of gridpoints
     n.append(n_boundary[i] +
@@ -35,63 +35,36 @@ for i in range(2):
              n_container[i] +
              n_boundary[i])
 
-    # distance step in medium
-    dd.append([d_boundary[i]/n_boundary[i],
-               d_container[i]/n_container[i],
-               d_honey[i]/n_honey[i],
-               d_container[i]/n_container[i],
-               d_boundary[i]/n_boundary[i]])
-
 # define masks
 honey_mask = np.zeros(n, dtype=bool)
 boundary_mask = np.ones(n, dtype=bool)
 container_mask = np.zeros(n, dtype=bool)
-# TODO make the numbers of masks calcualted by the computer instead of human
-honey_mask[6:16, 6:16] = True
+# TODO make the numbers in mask calcualted by the computer instead of human
+honey_mask[6:16, 6:26] = True
 boundary_mask[n_boundary[0]:-n_boundary[0],
               n_boundary[1]:-n_boundary[1]] = False
 container_mask = np.where(honey_mask, False, True)
 container_mask = np.where(boundary_mask, False, container_mask)
 
 # define positions
-dx_grid, dy_grid = np.zeros(n), np.zeros(n)
+dx, dy = np.zeros(n[1]), np.zeros(n[0])  # dx varies with y and vice versa
 
-dx_grid = np.where(honey_mask, d_honey[0]/n_honey[0], dx_grid)
-dx_grid = np.where(container_mask, d_container[0]/n_container[0], dx_grid)
-dx_grid = np.where(boundary_mask, d_boundary[0]/n_boundary[0], dx_grid)
+for i in range(len(dx)):
+    dx[i] = np.where(honey_mask[int(n[0]/2), i], d_honey[1]/n_honey[1], dx[i])
+    dx[i] = np.where(container_mask[int(n[0]/2), i],
+                     d_container[1]/n_container[1], dx[i])
+    dx[i] = np.where(boundary_mask[int(n[0]/2), i],
+                     d_boundary[1]/n_boundary[1], dx[i])
 
-dy_grid = np.where(honey_mask, d_honey[0]/n_honey[0], dy_grid)
-dy_grid = np.where(container_mask, d_container[0]/n_container[0], dy_grid)
-dy_grid = np.where(boundary_mask, d_boundary[0]/n_boundary[0], dy_grid)
+for i in range(len(dy)):
+    dy[i] = np.where(honey_mask[i, int(n[1]/2)], d_honey[0]/n_honey[0], dy[i])
+    dy[i] = np.where(container_mask[i, int(n[1]/2)],
+                     d_container[0]/n_container[0], dy[i])
+    dy[i] = np.where(boundary_mask[i, int(n[1]/2)],
+                     d_boundary[0]/n_boundary[0], dy[i])
 
 x_grid, y_grid = np.zeros(n), np.zeros(n)
-
-for i in range(n[0]):
-    dx_grid[i, :] = max(dx_grid[i, 1:-1])
-for j in range(n[1]):
-    dy_grid[:, j] = max(dy_grid[1:-1, j])
-
-
-for i in range(n[0]-1):
-    x_grid[i+1, :] += dx_grid[i, :]
-for j in range(n[1]):
-    dy_grid[:, j] = max(dy_grid[1:-1, j])
-
-
-x_grid[:] += dx_grid[:]
-
-
-
-# def main():
-#     honey = build()
-#     grid = put_in_pot(honey)
-#     set_boundarys()
-
-#     run_model()
-
-#     plot_result()  # is honey liquid and maxtemperature was never reached?
-#     plot_Temperaturedevelopement()
-
-# if __name__ == 'main':
-#     a = main()
-#     print(a)
+for i in range(n[1] - 1):
+    y_grid[:, i+1] = y_grid[:, i] + dx[i]
+for j in range(n[0]-1):
+    x_grid[j+1, :] = x_grid[j, :] + dy[j]
