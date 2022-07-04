470383adc442da696fc379844c0f6594d0a0fc78
PLSeuJ <jonathanschumann@posteo.de>
2022-07-04 22:28:44 +0200

    grid masks build

--- honey_heater/honey_heater_simulator_nooop.py
+++ honey_heater/honey_heater_simulator_nooop.py
@@ -14,127 +14,83 @@ Created on Fri Jun  3 17:42:09 2022
 """
 import numpy as np
 import matplotlib.pyplot as plt
-# import xarray as xr
+import xarray as xr
+
+# number of gridpoints in directions [y, x]
+n_honey = [10, 10]
+n_container = [5, 5]
+n_boundary = [1, 1]
+
+# distances in medium
+d_honey = [0.8, 0.4]
+d_container = [2 * 10**(-5), 2 * 10**(-5)]
+d_boundary = [10**(-3), 10**(-3)]
+
+n, dd = [], []
+for i in range(2):
+    # sum of gridpoints
+    n.append(n_boundary[i] +
+             n_container[i] +
+             n_honey[i] +
+             n_container[i] +
+             n_boundary[i])
+
+    # distance step in medium
+    dd.append([d_boundary[i]/n_boundary[i],
+               d_container[i]/n_container[i],
+               d_honey[i]/n_honey[i],
+               d_container[i]/n_container[i],
+               d_boundary[i]/n_boundary[i]])
+
+# define masks
+honey_mask = np.zeros(n, dtype=bool)
+boundary_mask = np.ones(n, dtype=bool)
+container_mask = np.zeros(n, dtype=bool)
+# TODO make the numbers of masks calcualted by the computer instead of human
+honey_mask[6:16, 6:16] = True
+boundary_mask[n_boundary[0]:-n_boundary[0],
+              n_boundary[1]:-n_boundary[1]] = False
+container_mask = np.where(honey_mask, False, True)
+container_mask = np.where(boundary_mask, False, container_mask)
+
+# define positions
+dx_grid, dy_grid = np.zeros(n), np.zeros(n)
+
+dx_grid = np.where(honey_mask, d_honey[0]/n_honey[0], dx_grid)
+dx_grid = np.where(container_mask, d_container[0]/n_container[0], dx_grid)
+dx_grid = np.where(boundary_mask, d_boundary[0]/n_boundary[0], dx_grid)
+
+dy_grid = np.where(honey_mask, d_honey[0]/n_honey[0], dy_grid)
+dy_grid = np.where(container_mask, d_container[0]/n_container[0], dy_grid)
+dy_grid = np.where(boundary_mask, d_boundary[0]/n_boundary[0], dy_grid)
+
+x_grid, y_grid = np.zeros(n), np.zeros(n)
+
+for i in range(n[0]):
+    dx_grid[i, :] = max(dx_grid[i, 1:-1])
+for j in range(n[1]):
+    dy_grid[:, j] = max(dy_grid[1:-1, j])
 
 
-# define grid from inside out
-def build_grid(name, shape=(10, 10), value=0):
-    """
-    test:
-    define dictionary item containing grid with specified initial value
-
-    Input:
-        grid        dict of grid
-        name        sting
-        add_shape   integer tupel ([..., z, y,] x)
-                        must match shape of old grid or have one value,
-                        added to all sides unless excluded.
-        value       flt or int
-        exclude     sides to exclude when adding gridpoints
-    Output:
-        dict(name: new grid)
-    """
-    grid = np.empty(shape,
-                    dtype=type(value) if type(value) == bool else float)
-    grid[:] = value
-    return {name: grid}
+for i in range(n[0]-1):
+    x_grid[i+1, :] += dx_grid[i, :]
+for j in range(n[1]):
+    dy_grid[:, j] = max(dy_grid[1:-1, j])
 
 
-def encase_grid(grid, add_shape=(2, 2), value=0, *, exclude=['none']):
-    """
-    test:
-    add gridpoints around your existing grid and return the entire thing
+x_grid[:] += dx_grid[:]
 
-    Input:
-        grid        numpy grid of the griddict
-        add_shape   integer tupel ([..., z, y,] x)
-                        must match shape of old grid or have one value,
-                        added to all sides unless excluded.
-        value       flt or int
-        exclude     sides to exclude when adding gridpoints
-                    use 'left', 'right', 'top', 'bottom' individualy or
-                    as a tupel.
-                    Default = None.
-                    Only tested for 2d cases.
-
-    Output:
-        new grid
-    """
-
-    def get_side(side, grid, value):
-        if side in ('left', 'right'):
-            shape = (grid.shape[0], add_shape[1])
-        if side in ('top', 'bottom'):
-            shape = (add_shape[0], grid.shape[1])
-
-        # make array and fill it with desired value
-        addgrid = np.empty(shape,
-                           dtype=type(value) if type(value) == bool else float)
-        addgrid[:] = value
-        return addgrid
-
-    def add_side(grid, addgrid, side_name):
-        if side == 'left': return np.concatenate((addgrid, grid), axis=1)
-        if side == 'right': return np.concatenate((grid, addgrid), axis=1)
-        if side == 'top': return np.concatenate((addgrid, grid), axis=0)
-        if side == 'bottom': return np.concatenate((grid, addgrid), axis=0)
-
-    # Do the thing
-    # add all sides individually to make it easly to exclude specified
-    for side in ('left', 'right', 'top', 'bottom'):
-        if (side in exclude):
-            continue
-        grid = add_side(grid, get_side(side, grid, value=value), side)
-    return grid
-
-
-# define the numerical grid for honey
-def build(shape=(10, 10),
-          add_shape
-              T=20,  # in °C
-              ):
-    """
-    sets up honey as numerical grid
-    returns dictonary of grids
-    """
-
-    # undergoing change in time
-    # physical parameters that (might) undergo change in numerical model.
-    honey = build_grid('Temperature', shape, T+273.15)
-
-    ng = build_grid('Temperature', shape, innershape, outervalue, innervalue)
-    ng = encase_grid(honey['Temperature'], add_shape, T+273.15)
-
-    honey.update(build_grid('heat capacity', shape, 0))
-    honey.update(build_grid('heat conductivity', shape, 0))
-    honey.update(build_grid('is_liquid', shape, False))  # physical state of honey
-
-    # constant stuff
-    honey.update(build_grid('is_honey', shape, True))
-    # to check for spoiled honey:
-    honey.update(build_grid('max_Temperature', shape, T+273.15))
-
-    # positional information for grid allocation in plotting
-    honey.update(build_grid('x', shape, 0))
-    honey.update(build_grid('y', shape, 0))
 
 
 # def main():
-    # honey = build()
-    # grid = put_in_pot(honey)
-    # set_boundarys()
+#     honey = build()
+#     grid = put_in_pot(honey)
+#     set_boundarys()
 
-    # run_model()
+#     run_model()
 
-    # plot_result()  # is honey liquid and maxtemperature was never reached?
-    # plot_Temperaturedevelopement()
-
-# define input parameters
-height = 0.8  # in m
-radius = 0.3  # in m
-wall_thickness = 0.46 / 100  # in mm
-wall_heat_transmissivity = 3
-
+#     plot_result()  # is honey liquid and maxtemperature was never reached?
+#     plot_Temperaturedevelopement()
 
 # if __name__ == 'main':
 #     a = main()
